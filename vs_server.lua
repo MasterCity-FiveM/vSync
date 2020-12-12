2d1d76794d1d8e2ba8a4b139edb1721ccbf63ca7
MasterkinG32 <lichwow_masterking@yahoo.com>
2020-12-12 02:25:19 -0800

    My own changes.

--- vs_server.lua
+++ vs_server.lua
@@ -1,23 +1,13 @@
+ESX                = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ------------------ change this -------------------
 
-admins = {
-    'steam:1100001158FFDF5',
-    'steam:11000013323b893',
-    'steam:110000108968cbd',
-}
-
 -- Set this to false if you don't want the weather to change automatically every 10 minutes.
-DynamicWeather = false
+DynamicWeather = true
 
 --------------------------------------------------
 debugprint = false -- don't touch this unless you know what you're doing or you're being asked by Vespura to turn this on.
 --------------------------------------------------
-
-
-
-
-
-
 
 -------------------- DON'T CHANGE THIS --------------------
 AvailableWeatherTypes = {
@@ -49,7 +39,7 @@ AddEventHandler('vSync:requestSync', function()
     TriggerClientEvent('vSync:updateWeather', -1, CurrentWeather, blackout)
     TriggerClientEvent('vSync:updateTime', -1, baseTime, timeOffset, freezeTime)
 end)
-
+--[[
 RegisterServerEvent('esx_doorlock:davetest69')
 AddEventHandler('esx_doorlock:davetest69', function()   
     blackout = not blackout
@@ -61,182 +51,82 @@ AddEventHandler('esx_doorlock:davetest69', function()
         TriggerClientEvent('chat:addMessage', -1, { template = '<div class="chat-message power"><b>LS Water & Power : </b> {1}</div>', args = { fal, " All problems resolved thank you for your patience " } })
     end
     TriggerEvent('vSync:requestSync')
-end)
+end)]]--
 
-function isAllowedToChange(player)
-    local allowed = false
-    for i,id in ipairs(admins) do
-        for x,pid in ipairs(GetPlayerIdentifiers(player)) do
-            if debugprint then print('admin id: ' .. id .. '\nplayer id:' .. pid) end
-            if string.lower(pid) == string.lower(id) then
-                allowed = true
-            end
-        end
-    end
-    return allowed
-end
+ESX.RegisterCommand('freezetime', 'admin', function(xPlayer, args, showError)
+	freezeTime = not freezeTime
+	if freezeTime then
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Time is now ~b~frozen~s~.')
+	else
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Time is ~y~no longer frozen~s~.')
+	end
+end, true, {help = "", validate = true})
 
-RegisterCommand('freezetime', function(source, args)
-    if source ~= 0 then
-        if isAllowedToChange(source) then
-            freezeTime = not freezeTime
-            if freezeTime then
-                TriggerClientEvent('vSync:notify', source, 'Time is now ~b~frozen~s~.')
-            else
-                TriggerClientEvent('vSync:notify', source, 'Time is ~y~no longer frozen~s~.')
-            end
-        else
-            TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1You are not allowed to use this command.')
-        end
-    else
-        freezeTime = not freezeTime
-        if freezeTime then
-            print("Time is now frozen.")
-        else
-            print("Time is no longer frozen.")
-        end
-    end
-end)
+ESX.RegisterCommand('freezeweather', 'admin', function(xPlayer, args, showError)
+	freezeTime = not freezeTime
+	DynamicWeather = not DynamicWeather
+	if not DynamicWeather then
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Dynamic weather changes are now ~r~disabled~s~.')
+	else
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Dynamic weather changes are now ~b~enabled~s~.')
+	end
+end, true, {help = "", validate = true})
 
-RegisterCommand('freezeweather', function(source, args)
-    if source ~= 0 then
-        if isAllowedToChange(source) then
-            DynamicWeather = not DynamicWeather
-            if not DynamicWeather then
-                TriggerClientEvent('vSync:notify', source, 'Dynamic weather changes are now ~r~disabled~s~.')
-            else
-                TriggerClientEvent('vSync:notify', source, 'Dynamic weather changes are now ~b~enabled~s~.')
-            end
-        else
-            TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1You are not allowed to use this command.')
-        end
-    else
-        DynamicWeather = not DynamicWeather
-        if not DynamicWeather then
-            print("Weather is now frozen.")
-        else
-            print("Weather is no longer frozen.")
-        end
-    end
-end)
+ESX.RegisterCommand('weather', 'admin', function(xPlayer, args, showError)
+	for i,wtype in ipairs(AvailableWeatherTypes) do
+		if wtype == string.upper(args.weather) then
+			validWeatherType = true
+		end
+	end
+	if validWeatherType then
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Weather will change to: ~y~' .. string.lower(args.weather) .. "~s~.")
+		CurrentWeather = string.upper(args.weather)
+		newWeatherTimer = 10
+		TriggerEvent('vSync:requestSync')
+	else
+		TriggerClientEvent('chatMessage', xPlayer.source, '', {255,255,255}, '^8Error: ^1Invalid weather type, valid weather types are: ^0\nEXTRASUNNY CLEAR NEUTRAL SMOG FOGGY OVERCAST CLOUDS CLEARING RAIN THUNDER SNOW BLIZZARD SNOWLIGHT XMAS HALLOWEEN ')
+	end
+end, true, {help = "", validate = true,arguments = {
+	{name = 'weather', help = '', type = 'string'}
+}})
 
-RegisterCommand('weather', function(source, args)
-    if source == 0 then
-        local validWeatherType = false
-        if args[1] == nil then
-            print("Invalid syntax, correct syntax is: /weather <weathertype> ")
-            return
-        else
-            for i,wtype in ipairs(AvailableWeatherTypes) do
-                if wtype == string.upper(args[1]) then
-                    validWeatherType = true
-                end
-            end
-            if validWeatherType then
-                print("Weather has been updated.")
-                CurrentWeather = string.upper(args[1])
-                newWeatherTimer = 10
-                TriggerEvent('vSync:requestSync')
-            else
-                print("Invalid weather type, valid weather types are: \nEXTRASUNNY CLEAR NEUTRAL SMOG FOGGY OVERCAST CLOUDS CLEARING RAIN THUNDER SNOW BLIZZARD SNOWLIGHT XMAS HALLOWEEN ")
-            end
-        end
-    else
-        if isAllowedToChange(source) then
-            local validWeatherType = false
-            if args[1] == nil then
-                TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1Invalid syntax, use ^0/weather <weatherType> ^1instead!')
-            else
-                for i,wtype in ipairs(AvailableWeatherTypes) do
-                    if wtype == string.upper(args[1]) then
-                        validWeatherType = true
-                    end
-                end
-                if validWeatherType then
-                    TriggerClientEvent('vSync:notify', source, 'Weather will change to: ~y~' .. string.lower(args[1]) .. "~s~.")
-                    CurrentWeather = string.upper(args[1])
-                    newWeatherTimer = 10
-                    TriggerEvent('vSync:requestSync')
-                else
-                    TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1Invalid weather type, valid weather types are: ^0\nEXTRASUNNY CLEAR NEUTRAL SMOG FOGGY OVERCAST CLOUDS CLEARING RAIN THUNDER SNOW BLIZZARD SNOWLIGHT XMAS HALLOWEEN ')
-                end
-            end
-        else
-            TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1You do not have access to that command.')
-            print('Access for command /weather denied.')
-        end
-    end
-end, false)
+ESX.RegisterCommand('blackout', 'admin', function(xPlayer, args, showError)
+	blackout = not blackout
+	if blackout then
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Blackout is now ~b~enabled~s~.')
+	else
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Blackout is now ~r~disabled~s~.')
+	end
+	TriggerEvent('vSync:requestSync')
+end, true, {help = "", validate = true})
 
-RegisterCommand('blackout', function(source)
-    if source == 0 then
-        blackout = not blackout
-        if blackout then
-            print("Blackout is now enabled.")
-        else
-            print("Blackout is now disabled.")
-        end
-    else
-        if isAllowedToChange(source) then
-            blackout = not blackout
-            if blackout then
-                TriggerClientEvent('vSync:notify', source, 'Blackout is now ~b~enabled~s~.')
-            else
-                TriggerClientEvent('vSync:notify', source, 'Blackout is now ~r~disabled~s~.')
-            end
-            TriggerEvent('vSync:requestSync')
-        end
-    end
-end)
+ESX.RegisterCommand('morning', 'admin', function(xPlayer, args, showError)
+	ShiftToMinute(0)
+	ShiftToHour(9)
+	TriggerClientEvent('vSync:notify', xPlayer.source, 'Time set to ~y~morning~s~.')
+	TriggerEvent('vSync:requestSync')
+end, true, {help = "", validate = true})
 
-RegisterCommand('morning', function(source)
-    if source == 0 then
-        print("For console, use the \"/time <hh> <mm>\" command instead!")
-        return
-    end
-    if isAllowedToChange(source) then
-        ShiftToMinute(0)
-        ShiftToHour(9)
-        TriggerClientEvent('vSync:notify', source, 'Time set to ~y~morning~s~.')
-        TriggerEvent('vSync:requestSync')
-    end
-end)
-RegisterCommand('noon', function(source)
-    if source == 0 then
-        print("For console, use the \"/time <hh> <mm>\" command instead!")
-        return
-    end
-    if isAllowedToChange(source) then
-        ShiftToMinute(0)
-        ShiftToHour(12)
-        TriggerClientEvent('vSync:notify', source, 'Time set to ~y~noon~s~.')
-        TriggerEvent('vSync:requestSync')
-    end
-end)
-RegisterCommand('evening', function(source)
-    if source == 0 then
-        print("For console, use the \"/time <hh> <mm>\" command instead!")
-        return
-    end
-    if isAllowedToChange(source) then
-        ShiftToMinute(0)
-        ShiftToHour(18)
-        TriggerClientEvent('vSync:notify', source, 'Time set to ~y~evening~s~.')
-        TriggerEvent('vSync:requestSync')
-    end
-end)
-RegisterCommand('night', function(source)
-    if source == 0 then
-        print("For console, use the \"/time <hh> <mm>\" command instead!")
-        return
-    end
-    if isAllowedToChange(source) then
-        ShiftToMinute(0)
-        ShiftToHour(23)
-        TriggerClientEvent('vSync:notify', source, 'Time set to ~y~night~s~.')
-        TriggerEvent('vSync:requestSync')
-    end
-end)
+ESX.RegisterCommand('noon', 'admin', function(xPlayer, args, showError)
+	ShiftToMinute(0)
+	ShiftToHour(12)
+	TriggerClientEvent('vSync:notify', xPlayer.source, 'Time set to ~y~noon~s~.')
+	TriggerEvent('vSync:requestSync')
+end, true, {help = "", validate = true})
+
+ESX.RegisterCommand('evening', 'admin', function(xPlayer, args, showError)
+	ShiftToMinute(0)
+	ShiftToHour(18)
+	TriggerClientEvent('vSync:notify', xPlayer.source, 'Time set to ~y~evening~s~.')
+	TriggerEvent('vSync:requestSync')
+end, true, {help = "", validate = true})
+
+ESX.RegisterCommand('night', 'admin', function(xPlayer, args, showError)
+	ShiftToMinute(0)
+	ShiftToHour(23)
+	TriggerClientEvent('vSync:notify', xPlayer.source, 'Time set to ~y~night~s~.')
+	TriggerEvent('vSync:requestSync')
+end, true, {help = "", validate = true})
 
 function ShiftToMinute(minute)
     timeOffset = timeOffset - ( ( (baseTime+timeOffset) % 60 ) - minute )
@@ -246,59 +136,36 @@ function ShiftToHour(hour)
     timeOffset = timeOffset - ( ( ((baseTime+timeOffset)/60) % 24 ) - hour ) * 60
 end
 
-RegisterCommand('time', function(source, args, rawCommand)
-    if source == 0 then
-        if tonumber(args[1]) ~= nil and tonumber(args[2]) ~= nil then
-            local argh = tonumber(args[1])
-            local argm = tonumber(args[2])
-            if argh < 24 then
-                ShiftToHour(argh)
-            else
-                ShiftToHour(0)
-            end
-            if argm < 60 then
-                ShiftToMinute(argm)
-            else
-                ShiftToMinute(0)
-            end
-            print("Time has changed to " .. argh .. ":" .. argm .. ".")
-            TriggerEvent('vSync:requestSync')
-        else
-            print("Invalid syntax, correct syntax is: time <hour> <minute> !")
-        end
-    elseif source ~= 0 then
-        if isAllowedToChange(source) then
-            if tonumber(args[1]) ~= nil and tonumber(args[2]) ~= nil then
-                local argh = tonumber(args[1])
-                local argm = tonumber(args[2])
-                if argh < 24 then
-                    ShiftToHour(argh)
-                else
-                    ShiftToHour(0)
-                end
-                if argm < 60 then
-                    ShiftToMinute(argm)
-                else
-                    ShiftToMinute(0)
-                end
-                local newtime = math.floor(((baseTime+timeOffset)/60)%24) .. ":"
-				local minute = math.floor((baseTime+timeOffset)%60)
-                if minute < 10 then
-                    newtime = newtime .. "0" .. minute
-                else
-                    newtime = newtime .. minute
-                end
-                TriggerClientEvent('vSync:notify', source, 'Time was changed to: ~y~' .. newtime .. "~s~!")
-                TriggerEvent('vSync:requestSync')
-            else
-                TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1Invalid syntax. Use ^0/time <hour> <minute> ^1instead!')
-            end
-        else
-            TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^8Error: ^1You do not have access to that command.')
-            print('Access for command /time denied.')
-        end
-    end
-end)
+ESX.RegisterCommand('time', 'admin', function(xPlayer, args, showError)
+	if tonumber(args.hour) ~= nil and tonumber(args.min) ~= nil then
+		local argh = tonumber(args.hour)
+		local argm = tonumber(args.min)
+		if argh < 24 then
+			ShiftToHour(argh)
+		else
+			ShiftToHour(0)
+		end
+		if argm < 60 then
+			ShiftToMinute(argm)
+		else
+			ShiftToMinute(0)
+		end
+		local newtime = math.floor(((baseTime+timeOffset)/60)%24) .. ":"
+		local minute = math.floor((baseTime+timeOffset)%60)
+		if minute < 10 then
+			newtime = newtime .. "0" .. minute
+		else
+			newtime = newtime .. minute
+		end
+		TriggerClientEvent('vSync:notify', xPlayer.source, 'Time was changed to: ~y~' .. newtime .. "~s~!")
+		TriggerEvent('vSync:requestSync')
+	else
+		TriggerClientEvent('chatMessage', xPlayer.source, '', {255,255,255}, '^8Error: ^1Invalid syntax. Use ^0/time <hour> <minute> ^1instead!')
+	end
+end, true, {help = "", validate = true, arguments = {
+	{name = 'hour', help = '', type = 'number'},
+	{name = 'min', help = '', type = 'number'}
+}})
 
 Citizen.CreateThread(function()
     while true do
@@ -333,7 +200,7 @@ Citizen.CreateThread(function()
             if DynamicWeather then
                 NextWeatherStage()
             end
-            newWeatherTimer = 10
+            newWeatherTimer = 30
         end
     end
 end)
